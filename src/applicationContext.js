2f23f08dcee77f456632b2b227993484b16c68c9
yvesyfzhang <yvesyfzhang@tencent.com>
2020-06-27 16:40:38 +0800

    add Life cycle function

--- src/applicationContext.js
+++ src/applicationContext.js
@@ -1,14 +1,20 @@
 import {
     NodeApplication,
-    Application
+    Application,
+    CombineMiddleWare,
+    yamlConfig
 } from '../lib/core';
 
 import './controllers/TestController'
 import './controllers/NewTestController'
 
 @Application
+@CombineMiddleWare()
 class ApplicationContext extends NodeApplication {
-
+    applicationBeforeMounted(app) {}
+    applicationMounted(...arg) {
+        console.log(yamlConfig)
+    }
 }
 
 export default ApplicationContext;
